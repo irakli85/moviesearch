860c5288208df52998313fce378563da0fcc9823
irakli <iberidze38@gmail.com>
2022-12-23 00:45:57 +0400

    some changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import loading from "./assets/sample.gif";
+import {Routes, Route} from "react-router-dom";
 
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
