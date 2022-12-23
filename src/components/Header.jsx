b1036c4cef5d20a11772a8e4d01162e37f3729e3
irakli85 <iberidze38@gmail.com>
2022-12-23 17:24:14 +0400

    final changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
-import {Link} from "react-router-dom";
 
 function Header() {
   return (
@@ -12,9 +12,7 @@ function Header() {
                     <option value="eng">English</option>
                     <option value="rus">русски</option>
                 </select>
-                <Link to="/signin">
-                  <button>sign in</button>
-                </Link>
+                  <button>sign in</button> 
             </div>
             <p className='screen'>Please use device with bigger screen resolution</p>
         </header>
