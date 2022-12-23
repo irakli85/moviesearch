5c1092519b50fc8aa00cddddc6b7a9fc2b2f3c6b
irakli85 <iberidze38@gmail.com>
2022-12-23 17:28:51 +0400

    some changes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    {/* <<BrowserRouter>
+    {/* <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}></Route>
         <Route path='/signin' element={<Signin/>}></Route>
       </Routes>
-    </BrowserRouter> >    */}
+    </BrowserRouter>     */}
     <App/>
   </>
 );
